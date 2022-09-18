d663c4cc9d22c4d9985d0472de950d7e46354bcd
Rony.Marroquin <ronymarroquin4991@gmail.com>
2022-09-17 19:23:12 -0600

    Actualizacion 2.0

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,21 @@
 fun main(parametro: Array<String>) {
+
+
     println(".....................................................")
     println(".... BIENVENIDO AL SISTEMA DE FARMACIA FIORELLA.....")
     println("........... FAVOR INGRESAR SUS CREDENCIALES ..........")
     println(".....................................................")
 
 
+//INICIO DE LOGIN
+
     val empleado = usuario()
 
+    empleado.carga()
     empleado.validacion()
     empleado.continuar()
+    empleado.cambiocontra()
+    empleado.menu()
 
-
-}
 
 
