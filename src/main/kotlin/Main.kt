bebeaa16637299cecf6af8ac1bfa9f648c5bf09d
edwin <edrenebonilla@gmail.com>
2022-09-18 09:39:34 -0600

    Inventario actualizado

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,28 +1,10 @@
 fun main() {
-    println("Bienvenido al catalogo")
-    println("Ingrese el numero de lo que busca")
-    println("1.productos en descuento")
+ val menu1= Menu()
 
-    println("2.Productos de conveniencia")
+    print(menu1.menu())
 
-    println("3.medicamentos")
 
-    println("4.salir")
-
-    var opt = readLine()!!.toInt();
-
-    when(opt){
-        1 -> {
-            println("productos en descuento")
-        }
-        2 -> {
-            println("Productos de conveniencia")
-        }
-        3 -> {
-            println("medicamentos")
-        }
-        4 -> {
-            println("salir")
-        }
-    }
 }
+
+
+
